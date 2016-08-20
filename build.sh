4c4d6d7ac0395d604543eb79396d72de06446f90
danielpinto8zz6 <danielpinto8zz6@gmail.com>
2016-08-20 23:35:48 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# USAGE: . build.sh codename
+if [ ! $1 ];
+then
+echo -e "";
+echo -e "USAGE: ./build.sh <device-codename>";
+echo -e ""
+else
+DEVICE="$1"
+. build/envsetup.sh
+lunch full_$DEVICE-userdebug
+export KBUILD_BUILD_HOST="android-build-system"
+mka otapackage
+fi
